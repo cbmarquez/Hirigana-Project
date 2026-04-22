cc16573f730f26d7cb283a477298738229a7aac8
cbmarquez <brianmarquez23@gmail.com>
2026-04-22 15:55:22 -0700

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,74 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5VHlrism45b":
+        Script1();
+        break;
+      case "6p00MBEQlwi":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('5y7YAp0gEay');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5v0RThN4Tgg';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5oEKl2QunWp');
+const duration = 750;
+const easing = 'ease-out';
+const id = '63ucmnkj11d';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
